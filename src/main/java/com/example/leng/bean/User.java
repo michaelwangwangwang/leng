8e7ec333cc973ef95bde4fd880ce7fddde58699b
mikewqwang <mikewqwang@tencent.com>
2020-08-10 22:50:47 +0800

    first commit

--- src/main/java/com/example/leng/bean/User.java
+++ src/main/java/com/example/leng/bean/User.java
@@ -13,4 +13,19 @@ public class User implements Serializable {
     public String username;
     public String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
